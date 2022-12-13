65974c245e6049e7bf0e12822c98e7980cf6b013
Gabriel <gabrielroger.chartroulle@gmail.com>
2022-12-12 19:52:58 -0500

    Fini et fonctionnel

--- src/Back/Connect4GameLogic.java
+++ src/Back/Connect4GameLogic.java
@@ -7,7 +7,7 @@ public class Connect4GameLogic {
     }
 
     public static boolean testTileWin(int x, int y, Connect4Board board) {
-        if (x >= 0 && x < board.length && y > 0 && y < board.length) {
+        if (x >= 0 && x < board.length && y >= 0 && y < board.length) {
             int team = board.getTile(x, y);
             if (y <= board.height - 4 && board.getTile(x, y + 1) == team && board.getTile(x, y + 2) == team && board.getTile(x, y + 3) == team) {
                 return true;
@@ -24,13 +24,13 @@ public class Connect4GameLogic {
             if (y >= 3 && board.getTile(x, y - 1) == team && board.getTile(x, y - 2) == team && board.getTile(x, y - 3) == team) {
             return true;
             }
-            if (x >= 4 && y <= board.height - 4 && board.getTile(x - 1, y + 1) == team && board.getTile(x - 2, y + 2) == team && board.getTile(x - 3, y + 3) == team) {
+            if (x >= 3 && y <= board.height - 4 && board.getTile(x - 1, y + 1) == team && board.getTile(x - 2, y + 2) == team && board.getTile(x - 3, y + 3) == team) {
                 return true;
             }
-            if (x >= 4 && board.getTile(x - 1, y) == team && board.getTile(x - 2, y) == team && board.getTile(x - 3, y) == team) {
+            if (x >= 3 && board.getTile(x - 1, y) == team && board.getTile(x - 2, y) == team && board.getTile(x - 3, y) == team) {
             return true;
             }
-            if (x >= 4 && y >= 3 && board.getTile(x - 1, y - 1) == team && board.getTile(x - 2, y - 2) == team && board.getTile(x - 3, y - 3) == team) {
+            if (x >= 3 && y >= 3 && board.getTile(x - 1, y - 1) == team && board.getTile(x - 2, y - 2) == team && board.getTile(x - 3, y - 3) == team) {
                 return true;
             }
 
